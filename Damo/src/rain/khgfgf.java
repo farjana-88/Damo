181721ef3343afa8a3acf4c3f4987653b3e91021
Farjana Bashar <farjana@Farjanas-MBP.home>
2022-11-05 07:26:28 -0400

    fggf

---
+++ Damo/src/rain/khgfgf.java
@@ -0,0 +1,14 @@
+package rain;
+
+public class khgfgf {
+
+	public khgfgf() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
